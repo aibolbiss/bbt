85df90cf841c4ab964c6914a21d2f35f1ed45484
Aibol Bissenkul <aibol.biss@gmail.com>
2024-06-23 04:15:15 +0500

    Add Prisma

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,43 +1,10 @@
-// import bcrypt from 'bcrypt';
-// import prisma from '../lib/prisma';
+import bcrypt from 'bcrypt';
 
-// export const register = async (req, res) => {
-//   const { username, email, password } = req.body;
-
-//   const hashedPassword = await bcrypt.hash(password, 10);
-//   //   const newUser = await prisma.user.create({
-//   //     data: {
-//   //       username,
-//   //       email,
-//   //       password: hashedPassword,
-//   //     },
-//   //   });
-
-//   //   console.log(newUser);
-
-//   //   res.status(201).json({ message: 'User created successfully' });
-
-//   //   try {
-//   //     const hashedPassword = await bcrypt.hash(password, 10);
-//   //     const newUser = await prisma.user.create({
-//   //       data: {
-//   //         username,
-//   //         email,
-//   //         password: hashedPassword,
-//   //       },
-//   //     });
-
-//   //     console.log(newUser);
-
-//   //     res.status(201).json({ message: 'User created successfully' });
-//   //   } catch (error) {
-//   //     console.log(error);
-//   //     res.status(500).json({ message: 'Failed to create user!' });
-//   //   }
-// };
-
-export const register = (req, res) => {
-  console.log(req.body);
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log(hashedPassword);
+  //   res.send('Register is working!');
 };
 
 export const login = (req, res) => {
