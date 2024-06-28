007799d0cd27485b390c2f65a2e9fca63054f755
Aibol Bissenkul <aibol.biss@gmail.com>
2024-06-28 17:03:54 +0500

    Add nodemon

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -1,11 +1,46 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './card.scss';
 import { useContext } from 'react';
 import { AuthContext } from '@/context/AuthContext';
+import apiRequest from '@/lib/apiRequest';
+
+// Aibol Bissenkul
 
 function Card({ item }) {
   const { currentUser } = useContext(AuthContext);
   const isOwner = currentUser?.id === item.userId;
+  const navigate = useNavigate();
+
+  const sendMessage = async () => {
+    if (!currentUser) {
+      navigate('/login');
+      return;
+    }
+    try {
+      // Получаем все чаты текущего пользователя
+      const res = await apiRequest.get('/chats');
+      const existingChat = res.data.find(
+        (chat) => chat.receiver.id === item.userId
+      );
+
+      if (existingChat) {
+        // Если существует, перенаправляем на существующий чат
+        navigate('/profile', {
+          state: { chatId: existingChat.id, receiver: item.user },
+        });
+      } else {
+        // Если не существует, создаем новый чат
+        const newChat = await apiRequest.post('/chats', {
+          receiverId: item.userId,
+        });
+        navigate('/profile', {
+          state: { chatId: newChat.data.id, receiver: item.user },
+        });
+      }
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  };
 
   return (
     <div className='card'>
@@ -116,6 +151,7 @@ function Card({ item }) {
                 <div
                   className='icon'
                   title='Написать'
+                  onClick={sendMessage}
                 >
                   <img
                     src='/mail.png'
